ff513e339f44aaac46ff1143145b054e0049d0d2
Daniil Dumchev <domdayd@ya.ru>
2022-08-24 14:53:23 +0500

    After review num

--- homework.py
+++ homework.py
@@ -13,28 +13,16 @@ class InfoMessage:
     calories: float
 
     def get_message(self) -> str:
-        """Предоставляет информацию о тренировке в виде строки."""
-        return (f'Тип тренировки: {self.training_type}; '
-                f'Длительность: {self.duration:.3f} ч.; '
-                f'Дистанция: {self.distance:.3f} км; '
-                f'Ср. скорость: {self.speed:.3f} км/ч; '
-                f'Потрачено ккал: {self.calories:.3f}.')
-
-    """Оставил старую функцию, потому что не могу понять,
-       как при использовании format вывести
-       определенное количество знаков после запятой"""
-
-    """def get_message(self) -> str:
         message: str = ('Тип тренировки: {0};'
-                        ' Длительность: {1: f} ч.;'
-                        ' Дистанция: {2: f} км;'
-                        ' Ср. скорость: {3: f} км/ч;'
-                        ' Потрачено ккал: {4: f}.').format(self.training_type,
-                                                           self.duration,
-                                                           self.distance,
-                                                           self.speed,
-                                                           self.calories)
-        return message"""
+                        ' Длительность: {1:.3f} ч.;'
+                        ' Дистанция: {2:.3f} км;'
+                        ' Ср. скорость: {3:.3f} км/ч;'
+                        ' Потрачено ккал: {4:.3f}.').format(self.training_type,
+                                                            self.duration,
+                                                            self.distance,
+                                                            self.speed,
+                                                            self.calories)
+        return message
 
 
 class Training:
